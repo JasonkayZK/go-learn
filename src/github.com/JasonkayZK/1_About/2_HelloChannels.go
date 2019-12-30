fe2aa8a627ad721192a2ed0f2ac0700b3bda7771
zk <271226192@qq.com>
2019-12-30 19:05:43 +0800

    Add ChannelDemo and Project Structure

---
+++ src/github.com/JasonkayZK/1_About/2_HelloChannels.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func printer(ch chan int) {
+	for i := range ch {
+		fmt.Printf("Received %d ", i)
+	}
+	wg.Done()
+}
+
+// main is the entry point for the program.
+func main() {
+	c := make(chan int)
+	go printer(c)
+	wg.Add(1)
+
+	// main is the entry point for the program.
+	for i := 1; i <= 10; i++ {
+		c <- i
+	}
+
+	close(c)
+	wg.Wait()
+}
+
