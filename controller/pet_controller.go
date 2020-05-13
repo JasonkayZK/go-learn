95d27256251df9b20485ce3eac899687d88b5c4e
zk <271226192@qq.com>
2020-05-13 23:46:39 +0800

    finished: Create & Show in controller

--- controller/pet_controller.go
+++ controller/pet_controller.go
@@ -27,11 +27,43 @@ func (p *PetController) Index(c *gin.Context) {
 }
 
 func (p *PetController) Create(c *gin.Context) {
+	var pet models.Pet
+	err := c.Bind(pet)
+	if err != nil {
+		c.JSON(404, gin.H{"error": err.Error()})
+		return
+	}
 
+	insert, err := p.DB.Table("pets").Insert(&pet)
+	if err != nil {
+		c.JSON(404, gin.H{"error": err.Error()})
+		return
+	}
+	if insert != 1 {
+		c.JSON(404, gin.H{"error": "Fail to insert, maybe exist?"})
+	}
+
+	c.JSON(200, gin.H{"status": 200, "message": "pet item created."})
 }
 
 func (p *PetController) Show(c *gin.Context) {
+	var pet models.Pet
+	has, err := p.DB.Table("pets").Where("id = ?", c.Param("id")).Get(&pet)
+	if err != nil {
+		c.JSON(404, gin.H {"status": 404, "message": "pet select error"})
+		return
+	}
+	if !has {
+		c.JSON(404, gin.H {"status": 404, "message": "pet not found"})
+		return
+	}
 
+	if pet.Id == 0 {
+		c.JSON(404, gin.H {"status": 404, "message": "pet not found"})
+		return
+	}
+
+	c.JSON(200, gin.H {"status": 200, "data": pet})
 }
 
 func (p *PetController) Update(c *gin.Context) {
