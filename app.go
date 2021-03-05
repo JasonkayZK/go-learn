ae7aa033b19effa0044c93a1e2ca6352b86e8cf9
JasonkayZK <271226192@qq.com>
2021-03-05 20:41:55 +0800

    finished: sse

--- app.go
+++ app.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"hello-go/goodbye"
-	"hello-go/hello"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"gopkg.in/antage/eventsource.v1"
 )
 
 func main() {
-	hello.SayHello()
-	goodbye.SayGoodbye()
+	es := eventsource.New(nil, nil)
+	defer es.Close()
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/events", es)
+	go func() {
+		for {
+			// 每2秒发送一条当前时间消息，并打印对应客户端数量
+			es.SendEventMessage(fmt.Sprintf("hello, now is: %s", time.Now()), "", "")
+			log.Printf("Hello has been sent (consumers: %d)", es.ConsumersCount())
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	log.Println("Open URL http://localhost:8080/ in your browser.")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module hello-go
+module sse-demo
 
 go 1.14
+
+require gopkg.in/antage/eventsource.v1 v1.0.0-20150318155416-803f4c5af225
